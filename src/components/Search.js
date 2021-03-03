74e811bedf52e188e543e50dfe6d75e870205dcb
jmarcossf <jamlmarcos@gmail.com>
2021-03-02 22:59:56 -0800

    Added renderedResults to return a list of wikipedia query results.

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('default');
+  const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
 
+  console.log('results:', results);
   // console.log('I run with EVERY render');
 
   // useEffect(() => {
@@ -17,7 +19,7 @@ const Search = () => {
   // });
   useEffect(() => {
     const search = async () => {   // RECOMMENDED WAY BY REACT
-      axios.get('https://en.wikipedia.org/w/api.php', {
+      const { data } = (await axios.get('https://en.wikipedia.org/w/api.php', {
         params: {
           action: 'query',
           list: 'search',
@@ -25,14 +27,36 @@ const Search = () => {
           format: 'json',
           srsearch: {term}
         }
-      });
+      }));
+      setResults(data.query.search);
     };
-    search()
+    if (term) {
+      search();
+    }
 
     // (async () => {  // alternative
     //   axios.get('adsfasdfasdf');
     // })();
   }, [term]);
+
+  const renderedResults = results.map((result, index) => {
+    return (
+      <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            className="ui button">Go</a>
+        </div>
+        <div className="content">
+          <div
+            className="header">
+            {result.title}
+          </div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    )
+  });
 
   return (
     <div>
@@ -44,8 +68,10 @@ const Search = () => {
             onChange={(e) => setTerm(e.target.value)}
             type="text" className="input"
           />
-          <p>{term}</p>
         </div>
+      </div>
+      <div className="ui celled list">
+        {renderedResults}
       </div>
     </div>
   );
