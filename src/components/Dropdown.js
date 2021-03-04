14fe0c51430f1c5d385f9a70f4562aec2df7c300
jmarcossf <jamlmarcos@gmail.com>
2021-03-03 16:26:30 -0800

    Added a useEffect() AND a useRef() to listen for click events and to determine if a click occured on the page OR if it occured on the item element itself.

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({options, selectedOption, onSelectedChange}) => {
-  const [open, setOpen] = useState(false)
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const ref = useRef();
+
+  // Setting an event listener
+  useEffect(() => {
+    document.body.addEventListener('click', (e) => {
+      // Using useRef as "ref" here to bypass setIsDropdownOpen
+      // if the element is the most parent element in this component
+      if (ref.current && ref.current.contains(e.target)) {
+        return;
+      }
+        setIsDropdownOpen(false);
+    }, { capture: true });
+  }, []);
+
   const renderedOptions = options.map((option) => {
     if (selectedOption.value === option.value) {
       return null;  // Do not show this item if it is the currently selected one
@@ -18,16 +32,18 @@ const Dropdown = ({options, selectedOption, onSelectedChange}) => {
   })
 
   return (
-    <div className="ui form">
+    <div
+      ref={ref}
+      className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
         <div
-          onClick={() => setOpen(!open)}
+          onClick={() => setIsDropdownOpen(!isDropdownOpen)}
           className={`ui selection dropdown ${options ? 'visible active' : ''}`}
         >
           <i className="dropdown icon"></i>
           <div className="text"> Selected: {selectedOption.label}</div>
-          <div className={`menu ${open ? 'visible transition' : ''}`}>
+          <div className={`menu ${isDropdownOpen ? 'visible transition' : ''}`}>
             {renderedOptions}
           </div>
         </div>
